32974e934be0f02f53a524779eee793b7d95fd4c
MihaiDragan1 <51019663+mihaidragan1@users.noreply.github.com>
2019-06-29 23:20:13 +0300

    refactor files

---
+++ src/b_22_06/RefactorFilesHomework1/RefactorPrimeNo.java
@@ -0,0 +1,39 @@
+package b_22_06.RefactorFilesHomework1;
+
+import java.util.Scanner;
+
+public class RefactorPrimeNo {
+
+
+    public static String PrimeNumber() {
+
+        Scanner scanner = new Scanner(System.in);
+        int i = 0;
+        int num = 0;
+        //Empty String
+        String primeNumbers = "";
+        System.out.println("Enter the value of n:");
+        int n = scanner.nextInt();
+        scanner.close();
+        for (i = 1; i <= n; i++) {
+            int counter = 0;
+            for (num = i; num >= 1; num--) {
+                if (i % num == 0) {
+                    counter = counter + 1;
+                }
+            }
+            if (counter == 2) {
+                //Appended the Prime number to the String
+                primeNumbers = primeNumbers + i + " ";
+            }
+        }
+        System.out.println("Prime numbers from 1 to n are :");
+
+    return primeNumbers;
+}
+    public static void main(String[] args) {
+        System.out.println(PrimeNumber());
+
+    }
+}
+
